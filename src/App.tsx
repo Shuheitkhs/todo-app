ea51ef94eb397d18ebdb37c000b17b7e1cc3879e
Stk <shuheitakahashi0822@gmail.com>
2024-08-19 11:30:22 +0900

    レビュー後の最初のコミット

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React, { useEffect } from 'react';
-import { useSetRecoilState } from 'recoil';
-import { todoListState , Todo } from './recoil/atoms';
-import AddTodo from './components/AddTodo';
-import TodoList from './components/TodoList';
-import Filters from './components/Filters';
+import React, { useEffect } from "react";
+import { useSetRecoilState } from "recoil";
+import { todoListState, Todo } from "./recoil/atoms";
+import AddTodo from "./components/AddTodo";
+import TodoList from "./components/TodoList";
+import Filters from "./components/Filters";
 // 以下でfirebaseと連携
-import { db } from './firebaseConfig';
-import { collection, getDocs } from 'firebase/firestore';
-import './App.css'
+import { db } from "./firebaseConfig";
+import { collection, getDocs } from "firebase/firestore";
+import "./App.css";
 
 const App: React.FC = () => {
   const setTodoList = useSetRecoilState(todoListState);
@@ -15,8 +15,13 @@ const App: React.FC = () => {
   // firebaseから情報を受け取って更新
   useEffect(() => {
     const fetchTodos = async () => {
-      const querySnapshot = await getDocs(collection(db, 'todos'));
-      const todos = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() } as Todo));
+      const querySnapshot = await getDocs(collection(db, "todos"));
+      // レビューでの修正前
+      // const todos = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() } as Todo));
+      // レビューでの修正後
+      const todos = querySnapshot.docs.map(
+        (doc) => ({ ...doc.data(), id: doc.id } as Todo)
+      );
       setTodoList(todos);
     };
 
@@ -25,7 +30,7 @@ const App: React.FC = () => {
   }, [setTodoList]);
 
   return (
-    <div className='App'>
+    <div className="App">
       <h1>TODOアプリ</h1>
       <AddTodo />
       <Filters />
